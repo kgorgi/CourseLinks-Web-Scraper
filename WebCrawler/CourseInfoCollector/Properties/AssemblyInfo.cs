c18f32f17fadf82fffc2090db236301de04eedca
Amandeep Singh <asingh@latitudegeo.com>
2017-10-28 19:51:03 -0700

    Added support for handling couse prereqs. Added Json support for CourseNode object. Added unit tests. Refactored code into a library project.

---
+++ WebCrawler/CourseInfoCollector/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CourseInfoCollector")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CourseInfoCollector")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("604ffd3d-41cc-4ebd-9822-4e4d7d77f36e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
