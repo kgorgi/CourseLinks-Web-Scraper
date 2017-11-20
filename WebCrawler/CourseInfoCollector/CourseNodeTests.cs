64b030f359fac9ba00eccaa6bae58bc009792dbd
Amandeep Singh <asingh@latitudegeo.com>
2017-11-19 17:17:43 -0800

    Fixed unit test.

--- WebCrawler/CourseInfoCollector/CourseNodeTests.cs
+++ WebCrawler/CourseInfoCollector/CourseNodeTests.cs
@@ -52,7 +52,7 @@ namespace CourseInfoCollector
 
             var jsonSting = JsonConvert.SerializeObject(courseNode);
 
-            string expectedJson = "{\"CourseCode\":\"255\",\"FieldOfStudy\":\"Math\",\"MarkUp\":\"HTML CONTENT HERE\",\"PreReqs\":[{\"courseIds\":[\"ENG210\"],\"UserFriendlyCondition\":\"Absolute\"},{\"NumberOfCourses\":1,\"courseIds\":[\"ENG150\",\"MATH160\"],\"UserFriendlyCondition\":\"Courses\"}],\"CoReqs\":[{\"courseIds\":[\"SENG321\"],\"UserFriendlyCondition\":\"Absolute\"}]}";
+            string expectedJson = "{\"CourseCode\":\"255\",\"FieldOfStudy\":\"Math\",\"MarkUp\":\"HTML CONTENT HERE\",\"PreReqs\":[{\"courseIds\":[\"ENG210\"],\"UserFriendlyCondition\":\"Absolute\"},{\"NumberOfCourses\":1,\"courseIds\":[\"ENG150\",\"MATH160\"],\"UserFriendlyCondition\":\"Courses\"}],\"CoReqs\":[{\"courseIds\":[\"SENG321\"],\"UserFriendlyCondition\":\"Absolute\"}],\"PreOrCoReqs\":null}";
 
             // ----- Expected Result -----
 
